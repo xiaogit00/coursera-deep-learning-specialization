b4e07ff16386cac2ab9051ecddca4d8895dad382
xiaogit00 <leibingguo@gmail.com>
2025-04-05 10:26:32 +0800

    some changes

---
+++ C1 - Neural Networks and Deep Learning/Week 3/Planar data classification with one hidden layer/logger_utils.py
@@ -0,0 +1,18 @@
+import numpy as np 
+
+def custom_sink(message):
+    record = message.record
+    function = record['function']
+    message = record['message']
+    extra = record['extra']
+    if extra.get("format") == "function_inputs":
+        args_strings = []
+        for k, v in extra.items():
+            if k == "format": continue
+            args_log = f"\n{k}: {v.shape if type(v) == np.ndarray else ''}\n {v} \n"
+            args_strings.append(args_log)
+        log_message = f"\n-----[{function}]::{message}" + "".join(args_strings)
+    else:
+        log_message = f"{function}:: {message} \n"
+    with open("file.log", "a") as file:
+        file.write(log_message)
